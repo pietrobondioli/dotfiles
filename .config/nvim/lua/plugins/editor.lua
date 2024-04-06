8f0f7e7c1f943a37d35da8b28b79dcb729fd901f
Pietro Bondioli <dev@pietrobondioli.com.br>
2024-04-05 21:44:34 -0300

    feat: tired of messing up with own config, setup LazyVim with custom config

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,525 +1,132 @@
-local Utils = require("utils")
-
-local map = Utils.safe_keymap_set
-
 return {
-	{
-		"kdheepak/lazygit.nvim",
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-	},
+  {
+    "sindrets/winshift.nvim",
+    config = function()
+      require("winshift").setup()
+    end,
+    keys = {
+      { "C-S-j>", "<cmd>WinShift down<CR>", desc = "Move window down" },
+      { "C-S-k>", "<cmd>WinShift up<CR>", desc = "Move window up" },
+      { "C-S-h>", "<cmd>WinShift left<CR>", desc = "Move window left" },
+      { "C-S-l>", "<cmd>WinShift right<CR>", desc = "Move window right" },
+    },
+  },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		version = false,
-		dependencies = {
-			"ThePrimeagen/harpoon",
-			"nvim-lua/plenary.nvim",
-			"kkharji/sqlite.lua",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-				cond = vim.fn.executable("cmake") == 1,
-			},
-			"nvim-telescope/telescope-live-grep-args.nvim",
-		},
-		config = function()
-			local actions = require("telescope.actions")
-			local telescope = require("telescope")
-			local lga_actions = require("telescope-live-grep-args.actions")
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      local oil = require("oil")
+      oil.setup({
+        columns = {
+          "icon",
+        },
+        default_file_explorer = true,
+        experimental_watch_for_changes = true,
+        use_default_keymaps = false,
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-\\>"] = "actions.select_vsplit",
+          ["<C-enter>"] = "actions.select_split", -- this is used to navigate left
+          ["<C-t>"] = "actions.select_tab",
+          ["<C-p>"] = "actions.preview",
+          ["<C-c>"] = "actions.close",
+          ["q"] = "actions.close",
+          ["<C-r>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = "actions.tcd",
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+        },
+        view_options = { show_hidden = true },
+        preview = {
+          max_width = 0.6,
+        },
+        float = {
+          max_width = 100,
+        },
+      })
+    end,
 
-			telescope.setup({
-				defaults = {
-					windblend = 10,
-					previewer = false,
-					prompt_title = false,
-					prompt_prefix = "🔍 ",
-					theme = "cursor",
-					layout_strategy = "vertical",
-					layout_config = {
-						width = 0.7,
-						vertical = {
-							prompt_position = "top",
-							mirror = true,
-						},
-					},
-					mappings = {
-						i = {
-							["<esc>"] = actions.close,
-							["<C-Down>"] = require("telescope.actions").cycle_history_next,
-							["<C-Up>"] = require("telescope.actions").cycle_history_prev,
-							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-							["<C-x>"] = actions.delete_buffer,
-						},
-					},
-					history = {
-						path = "~/.local/share/nvim/databases/telescope_history.sqlite3",
-						limit = 100,
-					},
-					file_ignore_patterns = {
-						"node_modules",
-						"yarn.lock",
-						".git",
-						".sl",
-						"_build",
-						".next",
-						"dist",
-						"vendor",
-						"*.min.*",
-						"*.map",
-						"bin",
-						"obj",
-						"*.o",
-					},
-					hidden = true,
-				},
-				extensions = {
-					ast_grep = {
-						command = { "sg", "--json=stream" }, -- must have --json=stream
-						grep_open_files = false, -- search in opened files
-						lang = nil, -- string value, specify language for ast-grep `nil` for default
-					},
-					live_grep_args = {
-						auto_quoting = true,
-						mappings = {
-							i = {
-								["<C-i>"] = lga_actions.quote_prompt(),
-								["<C-k>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-							},
-						},
-					},
-				},
-			})
+    vim.keymap.set("n", "<leader>o", "<cmd>lua require('oil').toggle_float()<cr>", { desc = "Toggle float" }),
+  },
 
-			-- Load Telescope extensions
-			local extensions_to_load = { "fzf", "dap", "smart_history", "live_grep_args", "harpoon" }
-			for _, extension in ipairs(extensions_to_load) do
-				pcall(telescope.load_extension, extension)
-			end
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 
-			map("n", "<leader>?", Utils.telescope("oldfiles"), { desc = "Find recently opened files" })
-			map("n", "<leader>:", Utils.telescope("command_history"), { desc = "Command History" })
-			map("n", "<leader>/", "<Cmd>Telescope live_grep_args<CR>", { desc = "Grep (root dir)" })
-			map("n", "<leader><space>", Utils.telescope("files"), { desc = "Find File" })
-			map(
-				"n",
-				"<leader>.",
-				-- require("telescope.builtin").current_buffer_fuzzy_find({ previewer = false }),
-				Utils.telescope("current_buffer_fuzzy_find", { previewer = false }),
-				{ desc = "Fuzzily search in current buffer" }
-			)
-			map("n", "<leader>;", Utils.telescope("resume"), { desc = "Resume last search" })
+  {
+    "hrsh7th/cmp-cmdline",
+    config = function()
+      local cmp = require("cmp")
+      -- `/` cmdline setup.
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
 
-			map("n", "<leader>sf", Utils.telescope("find_files", { hidden = true }), { desc = "Search Files" })
-			map("n", "<leader>sh", Utils.telescope("help_tags"), { desc = "Search Help" })
-			map(
-				"n",
-				"<leader>ss",
-				Utils.telescope("spell_suggest", { previewer = false }),
-				{ desc = "Search Spelling suggestions" }
-			)
-			map(
-				"n",
-				"<leader>sb",
-				Utils.telescope("buffers", { sort_mru = true, sort_lastused = true }),
-				{ desc = "Buffers" }
-			)
-			map("n", "<leader>sc", Utils.telescope("find_files", { cwd = Utils.root() }), { desc = "Find Config File" })
-			map("n", "<leader>sf", Utils.telescope("files"), { desc = "Find Files (root dir)" })
-			map("n", "<leader>sF", Utils.telescope("files", { cwd = false }), { desc = "Find Files (cwd)" })
-			map("n", "<leader>sg", Utils.telescope("git_files"), { desc = "Find Files (git-files)" })
-			map("n", "<leader>s-", Utils.telescope("oldfiles"), { desc = "Recent" })
-			map("n", "<leader>s_", Utils.telescope("oldfiles", { cwd = vim.loop.cwd() }), { desc = "Recent (cwd)" })
-			map("n", '<leader>s"', Utils.telescope("registers"), { desc = "Registers" })
-			map("n", "<leader>sa", Utils.telescope("autocommands"), { desc = "Auto Commands" })
-			map("n", "<leader>sb", Utils.telescope("current_buffer_fuzzy_find"), { desc = "Buffer" })
-			map("n", "<leader>sc", Utils.telescope("command_history"), { desc = "Command History" })
-			map("n", "<leader>sC", Utils.telescope("commands"), { desc = "Commands" })
-			map("n", "<leader>sd", Utils.telescope("diagnostics", { bufnr = 0 }), { desc = "Document diagnostics" })
-			map("n", "<leader>sD", Utils.telescope("diagnostics"), { desc = "Workspace diagnostics" })
-			map("n", "<leader>sg", Utils.telescope("live_grep"), { desc = "Grep (root dir)" })
-			map("n", "<leader>sG", Utils.telescope("live_grep", { cwd = false }), { desc = "Grep (cwd)" })
-			map("n", "<leader>sh", Utils.telescope("help_tags"), { desc = "Help Pages" })
-			map("n", "<leader>sH", Utils.telescope("highlights"), { desc = "Search Highlight Groups" })
-			map("n", "<leader>sk", Utils.telescope("keymaps"), { desc = "Key Maps" })
-			map("n", "<leader>sM", Utils.telescope("man_pages"), { desc = "Man Pages" })
-			map("n", "<leader>sm", Utils.telescope("marks"), { desc = "Jump to Mark" })
-			map("n", "<leader>so", Utils.telescope("vim_options"), { desc = "Options" })
-			map("n", "<leader>s.", Utils.telescope("resume"), { desc = "Resume" })
-			map(
-				"n",
-				"<leader>uC",
-				Utils.telescope("colorscheme", { enable_preview = true }),
-				{ desc = "Colorscheme with preview" }
-			)
-		end,
-	},
+      -- `:` cmdline setup.
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          {
+            name = "cmdline",
+            option = {
+              ignore_cmds = { "Man", "!" },
+            },
+          },
+        }),
+      })
+    end,
+  },
 
-	{
-		"AckslD/muren.nvim",
-		config = true,
-		keys = {
-			{
-				"<leader>sr",
-				function()
-					require("muren.api").open_ui()
-				end,
-				desc = "muren",
-			},
-		},
-	},
+  {
+    "telescope.nvim",
+    opts = {
+      defaults = {
+        history = {
+          path = "~/.local/share/nvim/databases/telescope_history.sqlite3",
+          limit = 100,
+        },
+      },
+    },
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        local telescope = require("telescope")
 
-	{
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		config = function()
-			local harpoon = require("harpoon")
+        -- Load Telescope extensions
+        local extensions_to_load = { "fzf", "dap", "smart_history" }
+        for _, extension in ipairs(extensions_to_load) do
+          pcall(telescope.load_extension, extension)
+        end
 
-			harpoon:setup()
-
-			map("n", "<leader>aa", function()
-				harpoon:list():append()
-			end, {
-				desc = "Append to Harpoon",
-			})
-			map("n", "<leader>ad", function()
-				harpoon:list():delete()
-			end, {
-				desc = "Delete from Harpoon",
-			})
-
-			map("n", "<leader>a1", function()
-				harpoon:list():select(1)
-			end, {
-				desc = "Select Harpoon 1",
-			})
-			map("n", "<leader>a2", function()
-				harpoon:list():select(2)
-			end, {
-				desc = "Select Harpoon 2",
-			})
-			map("n", "<leader>a3", function()
-				harpoon:list():select(3)
-			end, {
-				desc = "Select Harpoon 3",
-			})
-			map("n", "<leader>a4", function()
-				harpoon:list():select(4)
-			end, {
-				desc = "Select Harpoon 4",
-			})
-
-			-- Toggle previous & next buffers stored within Harpoon list
-			map("n", "<leader>a[", function()
-				harpoon:list():prev()
-			end, { desc = "Previous Harpoon" })
-			map("n", "<leader>a]", function()
-				harpoon:list():next()
-			end, { desc = "Next Harpoon" })
-			map("n", "[a", function()
-				harpoon:list():prev()
-			end, { desc = "Previous Harpoon" })
-			map("n", "]a", function()
-				harpoon:list():next()
-			end, { desc = "Next Harpoon" })
-
-			map("n", "<leader>,", "<Cmd>Telescope harpoon marks<CR>", { desc = "Harpoon" })
-			map("n", "<leader>a,", "<Cmd>Telescope harpoon marks<CR>", { desc = "Harpoon" })
-			map("n", "C-o", "<Cmd>Telescope harpoon marks<CR>", { desc = "Harpoon" })
-		end,
-	},
-
-	{
-		"sindrets/winshift.nvim",
-		config = function()
-			require("winshift").setup()
-		end,
-		keys = {
-			{ "C-S-j>", "<cmd>WinShift down<CR>", desc = "Move window down" },
-			{ "C-S-k>", "<cmd>WinShift up<CR>", desc = "Move window up" },
-			{ "C-S-h>", "<cmd>WinShift left<CR>", desc = "Move window left" },
-			{ "C-S-l>", "<cmd>WinShift right<CR>", desc = "Move window right" },
-		},
-	},
-
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-		keys = {
-			{
-				"s",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").jump()
-				end,
-				desc = "Flash",
-			},
-			{
-				"S",
-				mode = { "n", "x", "o" },
-				function()
-					require("flash").treesitter()
-				end,
-				desc = "Flash Treesitter",
-			},
-			{
-				"r",
-				mode = "o",
-				function()
-					require("flash").remote()
-				end,
-				desc = "Remote Flash",
-			},
-			{
-				"R",
-				mode = { "o", "x" },
-				function()
-					require("flash").treesitter_search()
-				end,
-				desc = "Treesitter Search",
-			},
-			{
-				"<c-s>",
-				mode = { "c" },
-				function()
-					require("flash").toggle()
-				end,
-				desc = "Toggle Flash Search",
-			},
-		},
-	},
-
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-
-	{
-		"stevearc/oil.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local oil = require("oil")
-			oil.setup({
-				columns = {
-					"icon",
-				},
-				default_file_explorer = true,
-				experimental_watch_for_changes = true,
-				use_default_keymaps = false,
-				keymaps = {
-					["g?"] = "actions.show_help",
-					["<CR>"] = "actions.select",
-					["<C-\\>"] = "actions.select_vsplit",
-					["<C-enter>"] = "actions.select_split", -- this is used to navigate left
-					["<C-t>"] = "actions.select_tab",
-					["<C-p>"] = "actions.preview",
-					["<C-c>"] = "actions.close",
-					["<C-r>"] = "actions.refresh",
-					["-"] = "actions.parent",
-					["_"] = "actions.open_cwd",
-					["`"] = "actions.cd",
-					["~"] = "actions.tcd",
-					["gs"] = "actions.change_sort",
-					["gx"] = "actions.open_external",
-					["g."] = "actions.toggle_hidden",
-				},
-				view_options = { show_hidden = true },
-				preview = {
-					max_width = 0.6,
-				},
-				float = {
-					max_width = 100,
-				},
-			})
-
-			map("n", "<leader>o", function()
-				oil.toggle_float()
-			end, { desc = "Open Oil" })
-		end,
-	},
-
-	{
-		"folke/persistence.nvim",
-		event = "BufReadPre", -- this will only start session saving when an actual file was opened
-		opts = { options = vim.opt.sessionoptions:get() },
-		keys = {
-			{
-				"<leader>qs",
-				function()
-					require("persistence").load()
-				end,
-				desc = "Restore Session",
-			},
-			{
-				"<leader>ql",
-				function()
-					require("persistence").load({ last = true })
-				end,
-				desc = "Restore Last Session",
-			},
-			{
-				"<leader>qd",
-				function()
-					require("persistence").stop()
-				end,
-				desc = "Don't Save Current Session",
-			},
-		},
-	},
-
-	{
-		"akinsho/toggleterm.nvim",
-		config = function()
-			require("toggleterm").setup({
-				open_mapping = [[<c-/>]],
-				shade_terminals = false,
-				-- add --login so ~/.zprofile is loaded
-				-- https://vi.stackexchange.com/questions/16019/neovim-terminal-not-reading-bash-profile/16021#16021
-				shell = "zsh --login",
-				direction = "horizontal",
-			})
-		end,
-	},
-
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			local trouble = require("trouble")
-			trouble.setup({
-				auto_preview = false,
-				auto_fold = true,
-				use_diagnostic_signs = true,
-			})
-
-			map("n", "<leader>xx", function()
-				trouble.toggle()
-			end, { desc = "Trouble Toggle" })
-
-			map("n", "<leader>xw", function()
-				trouble.toggle({ mode = "workspace" })
-			end, { desc = "Trouble Workspace" })
-
-			map("n", "<leader>xd", function()
-				trouble.toggle({ mode = "document" })
-			end, { desc = "Trouble Document" })
-
-			map("n", "<leader>xl", function()
-				trouble.toggle({ mode = "loclist" })
-			end, { desc = "Trouble Loclist" })
-
-			map("n", "<leader>xq", function()
-				trouble.toggle({ mode = "quickfix" })
-			end, { desc = "Trouble Quickfix" })
-
-			map("n", "gR", function()
-				trouble.toggle({ mode = "lsp_references" })
-			end, { desc = "Trouble LSP References" })
-		end,
-	},
-
-	{
-		"kevinhwang91/nvim-ufo",
-		event = "BufEnter",
-		dependencies = { "kevinhwang91/promise-async" },
-		config = function()
-			--- @diagnostic disable: unused-local
-			require("ufo").setup({
-				provider_selector = function(_bufnr, _filetype, _buftype)
-					return { "treesitter", "indent" }
-				end,
-			})
-		end,
-	},
-
-	-- Automatically highlights other instances of the word under your cursor.
-	-- This works with LSP, Treesitter, and regexp matching to find the other
-	-- instances.
-	{
-		"RRethy/vim-illuminate",
-		opts = {
-			delay = 200,
-			large_file_cutoff = 2000,
-			large_file_overrides = { providers = { "lsp" } },
-		},
-		config = function(_, opts)
-			require("illuminate").configure(opts)
-
-			local function map(key, dir, buffer)
-				vim.keymap.set("n", key, function()
-					require("illuminate")["goto_" .. dir .. "_reference"](false)
-				end, {
-					desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference",
-					buffer = buffer,
-				})
-			end
-
-			map("]]", "next")
-			map("[[", "prev")
-
-			-- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-			vim.api.nvim_create_autocmd("FileType", {
-				callback = function()
-					local buffer = vim.api.nvim_get_current_buf()
-					map("]]", "next", buffer)
-					map("[[", "prev", buffer)
-				end,
-			})
-		end,
-		keys = {
-			{ "]]", desc = "Next Reference" },
-			{ "[[", desc = "Prev Reference" },
-		},
-	},
-
-	-- buffer remove
-	{
-		"echasnovski/mini.bufremove",
-		keys = {
-			{
-				"<leader>bd",
-				function()
-					local bd = require("mini.bufremove").delete
-					if vim.bo.modified then
-						local choice =
-							vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
-						if choice == 1 then -- Yes
-							vim.cmd.write()
-							bd(0)
-						elseif choice == 2 then -- No
-							bd(0, true)
-						end
-					else
-						bd(0)
-					end
-				end,
-				desc = "Delete Buffer",
-			},
-			{
-				"<leader>bD",
-				function()
-					require("mini.bufremove").delete(0, true)
-				end,
-				desc = "Delete Buffer (Force)",
-			},
-		},
-	},
+        -- fuzzy find on current buffer leader + .
+        vim.keymap.set("n", "<leader>.", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { silent = true })
+      end,
+    },
+  },
 }
--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -1,55 +1,12 @@
 return {
-	{
-		"simrat39/symbols-outline.nvim",
-		keys = {
-			{ "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" },
-		},
-		cmd = "SymbolsOutline",
-		opts = function()
-			local Config = require("config.defaults")
-			local defaults = require("symbols-outline.config").defaults
-			local opts = { symbols = {}, symbol_blacklist = {} }
-			local filter = Config.kind_filter
+    {
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        build = function() vim.fn["mkdp#util#install"]() end,
+        cmd = {
+            "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"
+        }
+    },
 
-			if type(filter) == "table" then
-				filter = filter.default
-				if type(filter) == "table" then
-					for kind, symbol in pairs(defaults.symbols) do
-						opts.symbols[kind] = {
-							icon = Config.icons.kinds[kind] or symbol.icon,
-							hl = symbol.hl,
-						}
-						if not vim.tbl_contains(filter, kind) then
-							table.insert(opts.symbol_blacklist, kind)
-						end
-					end
-				end
-			end
-			return opts
-		end,
-	},
-
-	-- measure startuptime
-	{
-		"dstein64/vim-startuptime",
-		cmd = "StartupTime",
-		config = function()
-			vim.g.startuptime_tries = 10
-		end,
-	},
-
-	{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-
-	{
-		"iamcco/markdown-preview.nvim",
-		ft = "markdown",
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-		cmd = {
-			"MarkdownPreviewToggle",
-			"MarkdownPreview",
-			"MarkdownPreviewStop",
-		},
-	},
+    {"folke/todo-comments.nvim", dependencies = {"nvim-lua/plenary.nvim"}}
 }
