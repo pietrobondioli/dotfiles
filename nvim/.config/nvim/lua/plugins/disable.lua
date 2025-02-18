e9457f491d062ca1110cded6f1d1da4c69841df3
Pietro Bondioli <email@pietrobondioli.com.br>
2025-02-17 22:03:18 -0300

    feat: update lazyvim

--- nvim/.config/nvim/lua/plugins/disable.lua
+++ nvim/.config/nvim/lua/plugins/disable.lua
@@ -1,6 +1,3 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false, event = "VeryLazy", keys = false },
-  -- { "lukas-reineke/headlines.nvim", enabled = false },
-  -- { "iamcco/markdown-preview.nvim", enabled = false },
 }
