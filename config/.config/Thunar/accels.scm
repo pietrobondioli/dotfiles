5728e7289db2ca909af3446191640507cf090573
Pietro Bondioli <email@pietrobondioli.com.br>
2025-05-06 20:05:28 -0300

    feat: set x dpi and adjust font sizes

--- config/.config/Thunar/accels.scm
+++ config/.config/Thunar/accels.scm
@@ -117,6 +117,7 @@
 ; (gtk_accel_path "<Actions>/ThunarStandardView/duplicate" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/trash-delete-2" "KP_Delete")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/back-alt1" "BackSpace")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/998f7fc99aca3884918eff0090770fd9" "")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/create-folder" "<Primary><Shift>n")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-home" "<Alt>Home")
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-focused-split-view-pane" "")
